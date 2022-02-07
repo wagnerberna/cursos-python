023cba7fc10e55be4dcc0a0f7ddc8f505a2ae8ad
wagnerberna <cabelo29007@gmail.com>
2022-02-07 13:58:50 -0300

    dotenv and config Token

--- python_flask/07_RESTX_Mongo_Token_email_users_v4/app.py
+++ python_flask/07_RESTX_Mongo_Token_email_users_v4/app.py
@@ -3,14 +3,25 @@ from flask_restx import Api
 from flask_jwt_extended import JWTManager
 from src.controller.user import users_ns
 from src.controller.auth import auth_ns
-from src.service.message import *
 from blacklist import BLACKLIST
-import datetime
+# import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APP_PORT = os.getenv("APP_PORT")
+APP_CONFIG = os.getenv("APP_DEV")
+# APP_PROD
 
 app = Flask(__name__)
 
+# env:
+app.config.from_object(APP_CONFIG)
+# Classe:
+# app.config.from_pyfile('config.DevConfig')
+# Arquivo
 # app.config.from_pyfile('config.py')
-app.config.from_object('config_v2.DevConfig')
 
 # config Token
 # app.config["JWT_SECRET_KEY"] = "Udemy"
@@ -36,4 +47,4 @@ api.add_namespace(auth_ns, path='/')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=APP_PORT or 5000)
--- python_flask/07_RESTX_Mongo_Token_email_users_v4/src/service/mongo.py
+++ python_flask/07_RESTX_Mongo_Token_email_users_v4/src/service/mongo.py
@@ -1,12 +1,17 @@
 import pymongo
-from dotenv import dotenv_values
+import os
+from dotenv import load_dotenv
 
-config = dotenv_values(".env")
+load_dotenv()
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_PORT = int(os.getenv("MONGO_PORT"))
+
 
 class Mongo:
     def mongo_connect(self):
         try:
-            mongo = pymongo.MongoClient(host='localhost', port=27017, serverSelectionTimeoutMS=1000)
+            # print(MONGO_HOST, MONGO_PORT)
+            mongo = pymongo.MongoClient(host=MONGO_HOST, port=MONGO_PORT, serverSelectionTimeoutMS=1000)
             db = mongo.dcheroes
             mongo.server_info()  # trigger exception if cannot connect to db
             return db
